27f3029857332d8e7d724ac4320a5812d2a0ddf1
Charles Jeffery <charlesjeffery065@gmail.com>
2023-01-20 01:05:35 +1100

    did more cool stuff

---
+++ commands/reaction.js
@@ -0,0 +1,40 @@
+﻿const { SlashCommandBuilder } = require('discord.js');
+const { ActionRowBuilder, StringSelectMenuBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('react')
+		.setDescription('My live reaction to this information'),
+	async execute(interaction) {
+		const row = new ActionRowBuilder()
+			.addComponents(
+				new StringSelectMenuBuilder()
+					.setCustomId('reaction')
+					.setPlaceholder('Nothing selected')
+					.addOptions(
+						{
+							label: 'Stable 💀',
+							description: 'Stable? that\'s for horses',
+							value: 'stableSkull',
+						},
+						{
+							label: 'Sincerely Sorry',
+							description: 'I am sincerely sorry for cyberbullying',
+							value: 'sincerelySorry',
+						},
+						{
+							label: 'Bin Crown',
+							description: 'you dropped your crown king',
+							value: 'binCrown',
+						},
+						{
+							label: 'Goku 🤓',
+							description: 'nerd goku',
+							value: 'nerdGoku',
+						},
+					),
+			);
+
+		await interaction.reply({ content: 'Select witty reaction image:', ephemeral: true, components: [row] });
+	},
+};
---
+++ events/reactSelect.js
@@ -0,0 +1,23 @@
+const { AttachmentBuilder, Events } = require('discord.js');
+
+module.exports = {
+	name: Events.InteractionCreate,
+	async execute(interaction) {
+		if (!interaction.isStringSelectMenu()) return;
+
+		if (interaction.customId === 'reaction') {
+			const reactImage = new AttachmentBuilder('images/' + interaction.values[0] + '.png');
+
+			if (reactImage !== 'error') {
+				await interaction.update({ content: 'highly relevant and humourous image selected', components: [] });
+				await interaction.channel.send({ content: `${interaction.user.username}'s live reaction`, files: [reactImage] });
+			} else {
+				console.log('ERROR: File not found');
+				console.log(interaction.user);
+				console.log(interaction.values[0]);
+				await interaction.update({ content: 'you dumb shit you broke my god damn bot... how do you even manage I mean the file names are literally hard-coded like honestly this shouldn\'t even be possible yet here we are. anyways error logged. place a bug report on the [github](https://github.com/arles2464/discordbot/issues) please', components: [] });
+				await interaction.channel.send({ content: `ERROR: File not found. ${interaction.user.username}#${interaction.user.discriminator} broke the bot (rookie mistake)`, files: ['https://media.discordapp.net/attachments/1062978791745531974/1065620774448275546/epic-embed-fail.gif?width=576&height=491'] });
+			}
+		}
+	},
+};
